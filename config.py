ac9c1b8fdff3d6bec426c229f940f50528bfbddf
Vaibhav ka ultroid <97387093+Awesome-Vaibhav@users.noreply.github.com>
2024-01-23 20:43:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-api_id =  #my.telegram.org
-api_hash = "" #my.telegram.org
+api_id = "17520136" #my.telegram.org
+api_hash = "c1860784db86000a0eaec3f916d6d4ee" #my.telegram.org
 
 bot_token = ""
 
-CHAT_ID = -100 #chatid and make sure add bot on this chat and promote as admin 
+CHAT_ID = -4192276688 #chatid and make sure add bot on this chat and promote as admin 
 
 
 cookies = {
