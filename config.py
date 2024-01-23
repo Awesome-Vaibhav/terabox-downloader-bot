b0edfdc1c0e16621dc8fb6172c0b063e4f9795ce
Vaibhav ka ultroid <97387093+Awesome-Vaibhav@users.noreply.github.com>
2024-01-23 21:02:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 api_id = "17520136" #my.telegram.org
 api_hash = "c1860784db86000a0eaec3f916d6d4ee" #my.telegram.org
 
-bot_token = ""
+bot_token = "6974501396:AAEEO7cU9c0OlT5kYTDoL_-0E46KUl7U3uE"
 
 CHAT_ID = -4192276688 #chatid and make sure add bot on this chat and promote as admin 
 
